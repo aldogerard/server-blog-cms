7527ca6dd919377cf6900682137119a64659ba6a
aldogerard <aldogerard12@gmail.com>
2025-06-17 23:26:47 +0700

    fix : changer port to 8080

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import createAdmin from "./config/createAdmin.js";
 import "./utils/scheduler.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(helmet());
