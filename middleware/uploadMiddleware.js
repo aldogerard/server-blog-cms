106004d8733b9ab76c71157ffdab92738535146c
aldogerard <aldogerard12@gmail.com>
2025-06-30 11:51:40 +0700

    feat : add limit size multer

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,4 +1,12 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage();
-export const upload = multer({ storage });
+export const upload = multer({
+    storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // maksimal ukuran file: 5MB
+        fieldSize: 10 * 1024 * 1024, // maksimal ukuran field teks: 10MB (untuk HTML editor)
+        fields: 20, // maksimal jumlah total field
+        files: 1, // maksimal jumlah file
+    },
+});
